9d4b7614727600993576fd65e53d5a5fdd41bc32
hvzy <hadimirza1@gmail.com>
2020-12-16 23:56:55 -0500

    initial push

---
+++ scripts.js
@@ -0,0 +1,16 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+const mic = document.querySelector(".mic")
+let words = document.querySelector(".words")
+
+recognition.interimResults = true;
+
+mic.addEventListener("click", (event) => {
+    recognition.start();
+});
+
+recognition.addEventListener("result", (e) => {
+    // console.log(e)
+    words.innerHTML = e.results[0][0].transcript
+});
